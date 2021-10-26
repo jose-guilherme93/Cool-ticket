1d20a17d239b6717cbcf3ae355d401731682cf50
jose-guilherme93 <jose-guilherme93@hotmail.com>
2021-10-26 18:49:49 -0300

    added functios getUserByGithub and button.onclicl on main.js

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
 
 
-let users = 
-    {
-    github:'jose-guilherme93',
-    instagram:'jose_guilherme93',
-    youtube:'jose-guilherme93',
-    facebook:'jose-guilherme2015',
-}    
+// let users = 
+//     {
+//     github:'jose-guilherme93',
+//     instagram:'jose_guilherme93',
+//     youtube:'jose-guilherme93',
+//     facebook:'jose-guilherme2015',
+// }    
 
 
 
-function getUSerByGithub() {
-    
-let url = `https://api.github.com/users/${users.github}`
-    fetch(url)
-        .then(response => response.json())
-        .then(dataApi => {
-        description.textContent = dataApi.bio
-        userImage.src = dataApi.avatar_url
-    })
+
+let button = document.getElementById('button')
+
+let inputText = document.getElementById('inputText')
+
+
+button.onclick = ev => {
+    ev.preventDefault()
+    if (inputText.value) {
+        inputText.value
+    }
+    console.log(inputText.value)
+    // inputText.value = ''
     
 }
-getUSerByGithub()
 
+
+const inputText = 'jose-guilherme93'
+const getUserByGithub = async () => {
+    
+const response = await fetch(`https://api.github.com/users/${inputText}`)
+const dataApi = await response.json()
+    .then(dataApi => {
+    description.textContent = dataApi.bio
+    userImage.src = dataApi.avatar_url
+    
+})
+}
+getUserByGithub()
